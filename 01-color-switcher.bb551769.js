1ec88ee4e64addaf7dffc2bca44cddf0ab9b3507
v-duma <139060657+v-duma@users.noreply.github.com>
2023-11-26 15:24:57 +0000

    Deploying to gh-pages from @ v-duma/goit-js-hw-09@346c4c56a0be31603753d7c071f4b2f73b5dc8fd 🚀

---
+++ 01-color-switcher.bb551769.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(()=>{t.setAttribute("disabled",""),e.removeAttribute("disabled"),timerId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=`${t}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),e.setAttribute("disabled",""),clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.bb551769.js.map
